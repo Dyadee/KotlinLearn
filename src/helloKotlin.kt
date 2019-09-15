38c39a25b6f50253c8f2d514bc9ca61f081cfd28
jade <jade@gmail.com>
2019-09-15 18:14:01 +0800

    Using val and var in the program

--- src/helloKotlin.kt
+++ src/helloKotlin.kt
@@ -1,7 +1,13 @@
 fun main(args: Array<String>){
 
-    println("Hello, Jade!")
-    println("Welcome to Kotlin Programming")
 
 
+    val name: String = "Jade"
+    var age: Int = 37
+    val gender: String = "male"
+
+    println("Hello, $name!")
+    println("Welcome to Kotlin Programming.")
+
+    println("Your name is $name and you are a $gender of $age years old.")
 }
