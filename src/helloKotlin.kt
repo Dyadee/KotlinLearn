efb2d06ddef55bad1233b54c1e3a0eb96588983f
jade <jade@gmail.com>
2019-09-16 14:22:52 +0800

    simplifying a function in one line

--- src/helloKotlin.kt
+++ src/helloKotlin.kt
@@ -6,8 +6,20 @@ fun main(args: Array<String>){
     var age: Int = 37
     val gender: String = "male"
 
+    var a: Int = 32
+    var b: Int = 3
+
     println("Hello, $name!")
     println("Welcome to Kotlin Programming.")
 
     println("Your name is $name and you are a $gender of $age years old.")
+
+    fun max (a: Int, b: Int): Int{
+        return if (a>b) a else b
+    }
+
+    fun min(a: Int, b: Int): Int = if(a < b) a else b
+
+    println("The greater between $a and $b is ${max(a, b)}")
+    println("The least between $a and $b is ${min(a, b)}")
 }
